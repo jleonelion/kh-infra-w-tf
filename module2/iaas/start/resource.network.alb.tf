5f1d63c167a2f7b30053897cd562ad210878f1e4
jleonelion <jleonelion@gmail.com>
2023-01-20 05:10:31 +0000

    add server2 to alb target group

--- module2/iaas/start/resource.network.alb.tf
+++ module2/iaas/start/resource.network.alb.tf
@@ -33,3 +33,8 @@ resource "aws_alb_target_group_attachment" "server1" {
   target_id        = aws_instance.server1.id
   port             = 80
 }
+resource "aws_alb_target_group_attachment" "server2" {
+  target_group_arn = aws_lb_target_group.front_end.arn
+  target_id        = aws_instance.server2.id
+  port             = 80
+}
