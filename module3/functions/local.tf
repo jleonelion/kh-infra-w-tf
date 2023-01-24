3765fd244a0686a62ae6bff4dbea7f7f4ec83348
jleonelion <jleonelion@gmail.com>
2023-01-24 08:42:31 +0000

    initial add of function examples

---
+++ module3/functions/local.tf
@@ -0,0 +1,27 @@
+locals {
+  number1 = 1
+  number2 = 2
+
+  sentence     = "a simple sentence"
+  commas       = "string,with,commas"
+  base64string = "SGVsbG8gV29ybGQ="
+
+  map1 = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+  map2 = {
+    key2 = "value2"
+    key3 = "value3"
+  }
+
+  set1 = ["a", "b", "c"]
+  set2 = ["c", "d", "e"]
+
+  object1 = {
+    attribute1 = "value"
+    complex_attribute = {
+      attribute1 = "complex_value1"
+    }
+  }
+}
