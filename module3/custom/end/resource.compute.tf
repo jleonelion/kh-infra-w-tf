e83f9fccc3e2e4b815ed9c45b9c38682d62d8981
jleonelion <jleonelion@gmail.com>
2023-01-30 06:08:45 +0000

    initial add cusotm module excercise

---
+++ module3/custom/end/resource.compute.tf
@@ -0,0 +1,100 @@
+module "server1_name" {
+  source = "./module/naming"
+
+  app_abbrev  = var.prefix
+  name        = "server1"
+  region      = var.region
+  environment = var.environment
+  type        = "aws_instance"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "server1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.server_instance_type
+  key_name      = aws_key_pair.ssh_key.id
+  # put this server in subnet in zone a
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.server-sg.security_group_id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y && sudo apt install -y nginx
+  echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = module.server1_name.result
+  }
+
+  volume_tags = {
+    Name = module.server1_name.result
+  }
+}
+
+module "server2_name" {
+  source = "./module/naming"
+
+  app_abbrev  = var.prefix
+  name        = "server2"
+  region      = var.region
+  environment = var.environment
+  type        = "aws_instance"
+}
+
+resource "aws_instance" "server2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.server_instance_type
+  key_name      = aws_key_pair.ssh_key.id
+  # put this server in subnet in zone c
+  subnet_id              = module.vpc.public_subnets[1]
+  vpc_security_group_ids = [module.server-sg.security_group_id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y && sudo apt install -y nginx
+  echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = module.server2_name.result
+  }
+
+  volume_tags = {
+    Name = module.server2_name.result
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+module "ssh_key_name" {
+  source = "./module/naming"
+
+  app_abbrev  = var.prefix
+  name        = "sshkey"
+  region      = var.region
+  environment = var.environment
+  type        = "aws_key_pair"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
+resource "aws_key_pair" "ssh_key" {
+  key_name   = module.ssh_key_name.result
+  public_key = data.local_file.public_key.content
+}
+# https://registry.terraform.io/providers/hashicorp/local/latest/docs/data-sources/file
+data "local_file" "public_key" {
+  filename = "${path.module}/ec2_rsa.pub"
+}
